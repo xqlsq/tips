bb9d406b21108ec9242fa58b34deb316c2d3d9b5
xubo <xubo@jk.pingan-inc.cn>
2018-03-22 16:59:23 +0800

    microTask 更新

--- JS/JsThread/Microtask/index.js
+++ JS/JsThread/Microtask/index.js
@@ -10,6 +10,27 @@
 
 // }
 
+const observe = new MutationObserver(
+    () => {
+        console.log(1);
+    }
+);
+
+var ele = document.createElement('div');
+ele.setAttribute('boolean', 'true');
+
+observe.observe(ele, {
+    attributes: true
+});
+
+function onClick() {
+    setTimeout(() => console.log(3333), 0);
+    console.log(1111);
+    ele.setAttribute('boolean', ele.attributes.boolean === 'true' ? 'false' : 'true');
+    console.log(22222);
+}
+
+document.body.onclick = onClick;
 // 必须要写
 if (module.hot) {
     module.hot.accept();
